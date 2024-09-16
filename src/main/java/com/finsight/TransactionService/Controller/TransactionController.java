d55e87413952c79f2f48b05a2f741d8bdb257d86
Mark vd Molen <markmolenvd@gmail.com>
2024-09-16 19:55:59 +0200

    dummy request

--- src/main/java/com/finsight/TransactionService/Controller/TransactionController.java
+++ src/main/java/com/finsight/TransactionService/Controller/TransactionController.java
@@ -1,5 +1,24 @@
 package com.finsight.TransactionService.Controller;
+import com.finsight.TransactionService.Entity.Transaction;
+import com.finsight.TransactionService.Service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/transactions")
 public class TransactionController {
-    //BIED DE END POINTS AAN
+
+    @Autowired
+    private TransactionService transactionService;
+
+    // Endpoint om alle transacties op te halen
+    @GetMapping("/all")
+    public ResponseEntity<List<Transaction>> getAllTransactions() {
+        List<Transaction> transactions = transactionService.getAllTransactions();
+        return ResponseEntity.ok(transactions);
+    }
 }
--- src/main/java/com/finsight/TransactionService/Service/TransactionService.java
+++ src/main/java/com/finsight/TransactionService/Service/TransactionService.java
@@ -1,4 +1,26 @@
 package com.finsight.TransactionService.Service;
 
+import com.finsight.TransactionService.Entity.Transaction;
+import com.finsight.TransactionService.Repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TransactionService {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    // Methode om alle transacties op te halen
+    public List<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
+
+    // Methode om een transactie op te halen op basis van het ID
+    public Optional<Transaction> getTransactionById(Long id) {
+        return transactionRepository.findById(id);
+    }
 }
