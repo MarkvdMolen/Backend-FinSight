a783064690afc523980a64346c5e8b0104ae7895
Mark van der Molen <markmolenvd@gmail.com>
2025-09-17 19:05:15 +0200

    Use JDBC for custom ETL query

--- src/main/java/com/finsight/GroupedTransactionService/Controller/GroupedTransactionController.java
+++ src/main/java/com/finsight/GroupedTransactionService/Controller/GroupedTransactionController.java
@@ -1,9 +1,15 @@
 package com.finsight.GroupedTransactionService.Controller;
 
+import com.finsight.GroupedTransactionService.Entity.GroupedTransactionDTO;
 import com.finsight.GroupedTransactionService.Service.GroupedTransactionService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -16,5 +22,16 @@ public class GroupedTransactionController {
 
     @Autowired
     private GroupedTransactionService groupedTransactionService;
+
+    /**
+     * Trigger ETL
+     */
+    @PostMapping("/etl")
+    public ResponseEntity<Map<String, String>> runEtl() {
+        groupedTransactionService.runGroupedTransactionsETL();
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "OK");
+        return ResponseEntity.ok(response);
+    }
 
 }
